020578c2e863a99deb6c74941dde4bf0b8cbdce6
saru3450 <9971@holbertonstudents.com>
2025-01-17 13:56:01 +0000

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    arg = len(argv)
+    result = 0
+    for i in range(1, arg):
+        result +=int(argv[i])
+    print(result)
