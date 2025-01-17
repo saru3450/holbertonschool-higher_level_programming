448695a4a3c0b9fad2613fc24e1e5b290addae14
saru3450 <9971@holbertonstudents.com>
2025-01-17 13:18:48 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     arg = len(argv)
 
     if arg == 1:
-        print("0 argument.")
+        print("0 arguments.")
     elif arg == 2:
         print("1 argument:")
     elif arg > 2:
-        print(f"{arg - 1}argument:")
+        print(f"{arg - 1} arguments:")
 
 for i in range(1, arg):
     print("{}: {}".format(i, argv[i]))
