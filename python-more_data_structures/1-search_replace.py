9a42272eb74af2ef46141b60c2d353fac60cb018
saru3450 <9971@holbertonstudents.com>
2025-01-22 09:59:28 +0000

    1-search_replace.py

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    mon_list = my_list.copy()
+    for i in range(len(mon_list)):
+            if mon_list[i] == search:
+                mon_list[i] = replace
+    return mon_list
