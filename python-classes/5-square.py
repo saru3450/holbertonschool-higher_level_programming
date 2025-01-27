68bbd9f2b46e37dbe6804f8bdc2348ce24d19c4d
saru3450 <9971@holbertonstudents.com>
2025-01-27 23:34:51 +0000

    5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''
+define a Square
+'''
+
+
+class Square:
+    '''Empty classe Square'''
+
+    def __init__(self, size=0):
+        '''Private instance with size'''
+        self.__size = size
+
+        '''Getter and setter methods are not 100% Python'''
+    @property
+    def size(self):
+        '''property getter'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''property sette'''
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        '''
+        returns the current square area
+        '''
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        '''
+        Public instance method
+        '''
+        if self.__size == 0:
+            print()
+        for item in range(self.__size):
+           [print("#", end="") for j in range(self.__size)]
+           print ()
