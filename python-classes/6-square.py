64af3e963d617c6a1090ff99ff13a7399e87a173
saru3450 <9971@holbertonstudents.com>
2025-01-30 09:36:47 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
 '''
-define a Square
+Define a Square
 '''
 
 
 class Square:
-    '''Empty classe Square'''
+    '''Represents a square'''
 
     def __init__(self, size=0, position=(0, 0)):
-        '''Private instance with size'''
-        self.__size = size
-        self.__position = position
+        '''Initialize the square'''
+        self.size = size
+        self.position = position
 
-    ''' Instantiation with optional '''
+    @property
+    def size(self):
+        '''Getter for size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Setter for size'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     @property
     def position(self):
+        '''Getter for position'''
         return self.__position
 
     @position.setter
     def position(self, value):
-        ''' stter of position'''
+        '''Setter for position'''
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
@@ -27,38 +41,18 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-        '''Getter and setter methods are not 100% Python'''
-    @property
-    def size(self):
-
-        '''property getter'''
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        '''property sette'''
-
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = value
-
     def area(self):
-        '''
-        returns the current square area
-        '''
-        return (self.__size * self.__size)
+        '''Return the current square area'''
+        return self.__size * self.__size
 
     def my_print(self):
-        '''
-        Public instance method
-        '''
+        '''Print the square with # and handle position'''
         if self.__size == 0:
             print()
             return
-        [print() for i in range(0, self.__position[1])]
+
+        # Print vertical spaces
+        [print() for _ in range(self.__position[1])]
 
         for _ in range(self.__size):
             print(" " * self.__position[0], end="")
