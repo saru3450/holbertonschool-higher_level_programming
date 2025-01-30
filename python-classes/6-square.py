b4e1dc1efeaea03632531566cc9512aa5b9e3552
saru3450 <9971@holbertonstudents.com>
2025-01-30 09:28:14 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -59,7 +59,7 @@ class Square:
             print()
             return
         [print() for i in range(0, self.__position[1])]
-        for i in range(self.__size):
-            [print("#", end="") for item in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
