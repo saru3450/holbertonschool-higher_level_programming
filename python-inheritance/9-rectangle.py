46c64406f253755f9846eb2f6f5aa1b49d5b7dd9
saru3450 <9971@holbertonstudents.com>
2025-02-07 13:39:45 +0000

    9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle(BaseGeometry):
     """
     class Rectangle that inherits from BaseGeometry
     """
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
