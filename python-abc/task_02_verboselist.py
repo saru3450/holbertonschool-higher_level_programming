c7df453fd926a8bb724dc2c4380ef97489f50366
saru3450 <9971@holbertonstudents.com>
2025-02-07 13:56:29 +0000

    task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python3
-"""
-Create a class named VerboseList :
-inherits from the built-in list class.
-"""
-
+#!/usr/bin/python3
 
 class VerboseList(list):
-    """
-    class
-    """
-
     def append(self, item):
         print("Added [{}] to the list.".format(item))
         super().append(item)
 
     def extend(self, items):
-        print("Extended the list with [{}] items.".format(len(items)))
+        print("Extended the list with {} items.".format(items))
         super().extend(items)
 
     def remove(self, item):
         print("Removed [{}] from the list.".format(item))
         super().remove(item)
 
-    def pop(self, item=-1):
-        print("Popped [{}] from the list.".format(self[item]))
-        super().pop(item)
-        return super().pop(item)
+    def pop(self, index=-1):
+        print("Popped [{}] from the list.".format(index))
+        return super().pop(index)
