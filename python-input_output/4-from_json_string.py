7a2aa660316b08ccdb6a7a0369b59cb9acf24c3b
saru3450 <9971@holbertonstudents.com>
2025-02-10 13:20:02 +0000

    4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that appends a string at the end
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns the JSON representation
+    """
+    return json.loads(my_str)
