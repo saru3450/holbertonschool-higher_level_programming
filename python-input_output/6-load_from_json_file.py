9c8e0143e3db688978e85a11a4fd9bc866529e79
saru3450 <9971@holbertonstudents.com>
2025-02-10 13:47:52 +0000

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+ writes an Object to a text file
+ """
+import json
+
+
+def load_from_json_file(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
