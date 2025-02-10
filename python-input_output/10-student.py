7352f0316158d314cb4b4011ef9af5410ed9a2ed
saru3450 <9971@holbertonstudents.com>
2025-02-10 22:42:40 +0000

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """
-first_name
-last_name
-age
+Write the class Student with intance:
+- first_name
+- last_name
+- age
 """
 
 
 class Student:
+    """
+    the class Student
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-     def to_json(self, attrs=None):
-         if attrs is None:
+    def to_json(self, attrs=None):
+        if attrs is None:
             return self.__dict__
         return {x: y for x, y in self.__dict__.items() if x in attrs}
