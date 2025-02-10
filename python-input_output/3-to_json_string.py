f123b8917170e509b24c5084b18b2d29ff050939
saru3450 <9971@holbertonstudents.com>
2025-02-10 13:12:37 +0000

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that appends a string at the end
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    returns the JSON representation
+    """
+    return json.dumps(my_obj)
