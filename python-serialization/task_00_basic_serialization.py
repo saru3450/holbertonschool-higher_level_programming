9f35d29af065501855b9269274d6c341ece0d14d
saru3450 <9971@holbertonstudents.com>
2025-02-11 12:23:27 +0000

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,8 +9,9 @@ def serialize_and_save_to_file(data, filename):
     """
     function serialize_and_save_from_to_file
     """
-     with open(filename, 'w') as file:
-         json.dump(data, file)
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
 
 def load_and_deserialize(filename):
     """
