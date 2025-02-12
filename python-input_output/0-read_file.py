19fc5a02a4bf7a68b9b5797f6a76f0996fa37ed8
saru3450 <9971@holbertonstudents.com>
2025-02-12 13:54:46 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,6 @@ that reads a text file (UTF8)
 
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
+        f.read = file.read()
         print(f.read(), end="")
+        file.close()
