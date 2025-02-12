06aa1ca7c999f4912c0592a6a2fac6f48f5b1395
saru3450 <9971@holbertonstudents.com>
2025-02-12 14:00:35 +0000

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,8 @@ string to a text file (UTF8)
 
 
 def write_file(filename="", text=""):
+    """
+     function that writes a string
+     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
